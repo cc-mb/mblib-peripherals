8e817542ef95ad1a1656e7e6845886abdac446b4
Milan Borovy <milan.borovy@bisimulations.com>
2024-11-25 19:09:00 +0100

    Add table merge

--- mb/algorithm/table.lua
+++ mb/algorithm/table.lua
@@ -20,4 +20,18 @@ function Table.contains_value(table, value)
   return false
 end
 
+--- Merges all passed table into one table. In case there's conflicting key present in multiple tables, the latest one will be used.
+---@param ... table Tables to merge.
+---@return table Merged table.
+function Table.merge(...)
+  local result = {}
+  for _, table in ipairs({...}) do
+    for k, v in pairs(table) do
+      result[k] = v
+    end
+  end
+
+  return result
+end
+
 return Table
